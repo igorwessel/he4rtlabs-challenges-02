c1aa0ad3f8742c5080cd84f17784554c36b84a24
igorwessel <igor_wessel@hotmail.com>
2020-03-22 06:52:50 -0300

    feat: Finish main animation, working in main content.
    Insert logo for github, twitter, animation in home finished, defining layout.

--- src/assets/js/animation.js
+++ src/assets/js/animation.js
@@ -1,12 +1,35 @@
-const logoPath = anime.path('.header-logo-path path')
+// Logo animation
+const logo = anime.timeline({
+  easing: 'easeInOutSine',
+  duration: 1500
+})
 
+logo.add({
+  targets: 'path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  fill: {
+    value: '#9163cc',
+    duration: 500,
+    delay: 850
+  },
+  delay: function(el, i) { return i * 250 },
+  direction: 'normal'
+}).add({
+  targets: 'path',
+  fill: '#FFFFFF'
+}).add({
+  targets: 'svg',
+  width: '60px'
+}, 1500).add({
+  targets: 'header',
+  backgroundColor: '#9163cc'
+}, '-=600')
+
+// Show after logo animation finish
 anime({
-    targets: 'path',
-    width: '200px',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
-    duration: 1500,
-    delay: function(el, i) { return i * 250 },
-    direction: 'alternate',
-    loop: true
-  });
+  targets: ['main', 'footer'],
+  opacity: [0, 1],
+  easing: 'easeInOutSine',
+  duration: 1000,
+  delay: 2000
+})
