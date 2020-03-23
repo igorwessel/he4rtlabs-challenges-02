b4da562f45821ac028542f1a8587efd6e25dd4ed
igorwessel <igor_wessel@hotmail.com>
2020-03-23 06:48:43 -0300

    feat: Working in add feature to table.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,16 @@
-// Este será o objeto principal no qual você irá se basear para as funções do desafio!
-// Caso haja dúvidas de como prosseguir, favor consultar a sala #js no nosso discord!
+const modalAddFeature = document.querySelector('.modal-add-feature')
+const btnOpenModal = document.querySelector('#btn-open-modal-features')
+const valuePerHour = document.querySelector('#main-input-value-per-hour')
+const featuresTableBody = document.querySelector('#main-list-features-table-body')
+const form = document.querySelector('#modal-feature-form')
+
+const calcFeature = (devHours, testHours) => {
+  let valuePerHourText = valuePerHour.value
+  valuePerHourText = valuePerHourText.replace(',', '.')
+
+  return eval(valuePerHourText * (parseInt(devHours) + parseInt(testHours))).toFixed(2)
+}
+
 let features = [
   {
     feature: "Authentication",
@@ -8,12 +19,27 @@ let features = [
   }
 ];
 
+const createRowTable = (data) => {
+  return `
+  <tr>
+    <td>${data.feature}</td>
+    <td>${data.devHours}</td>
+    <td>${data.testHours}</td>
+    <td>R$${calcFeature(data.devHours, data.testHours)}</td>
+  </tr>
+  `
+}
 
-// Dica: faça o layout e depois pense em como vai funcionar o script.
-
-
-const modalAddFeature = document.querySelector('.modal-add-feature')
-const btnOpenModal = document.querySelector('#btn-open-modal-features')
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  let formData = new FormData(form)
+  let feature = {}
+  formData.forEach( (value, key) => {
+    feature[key] = value
+  })
+  features.push(feature)
+  featuresTableBody.insertAdjacentHTML('beforeend', createRowTable(feature))
+})
 
 btnOpenModal.addEventListener('click', () => {
   modalAddFeature.style.display = 'block'
@@ -28,7 +54,12 @@ window.onclick = function(event) {
 // utils functions
 function isNumber(event) {
   let keycode = event.keyCode;
-  if (keycode > 47 && keycode < 59) {
+  const point = 46
+  const comma = 44
+  if (keycode == comma || keycode == point){
+    return true
+  }
+  if (keycode > 47 && keycode < 59 || keycode == point) {
       return true
   } else {
       return false
